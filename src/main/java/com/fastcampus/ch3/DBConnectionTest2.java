80ad44492845d908d8f318ef15269961e42b8720
oni <rainbow2774@naver.com>
2022-07-21 23:50:19 +0900

    spring jdbc 연결방법, 간단한 sql연습 복습

--- src/main/java/com/fastcampus/ch3/DBConnectionTest2.java
+++ src/main/java/com/fastcampus/ch3/DBConnectionTest2.java
@@ -9,7 +9,8 @@ import java.sql.Connection;
 public class DBConnectionTest2 {
 //    @Test
     public void jdbcConnectionTest() throws Exception {
-//        // 스키마의 이름(springbasic)이 다른 경우 알맞게 변경 , 대신 이렇게하면 데이터베이스 정보가 바뀔때마다 고치고 컴파일을 다시해야해서 불편해! 그래서
+//        // 스키마의 이름(springbasic)이 다른 경우 알맞게 변경 , 대신 이렇게하면 데이터베이스 정보가 바뀔때마다 고치고 컴파일을 다시해야해서 불편해! 그래서 아래와 같은방법으로 한다.
+//        직접 객체를 생성하는 방법
 //        String DB_URL = "jdbc:mysql://localhost:3306/springbasic?useUnicode=true&characterEncoding=utf8";
 //
 //        // DB의 userid와 pwd를 알맞게 변경
@@ -23,7 +24,7 @@ public class DBConnectionTest2 {
 //        ds.setUsername(DB_USER);
 //        ds.setPassword(DB_PASSWORD);
 
-        //이렇게 set을 빈으로 등록하고 가져다 쓴다.
+        //root-context에다가 접속정보를 주고 getBean으로 얻어온다.
         ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
         DataSource ds = ac.getBean(DataSource.class);
 
